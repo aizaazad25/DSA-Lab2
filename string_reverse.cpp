addb616cc936e089d7f71795b044024818718e41
Aiza Azad <aizaazad25@gmail.com>
2026-09-18 09:56:44 +0500

    Add string reverse program

---
+++ string_reverse.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main() {
+    int size;
+
+    cout << "Enter the size of the string: ";
+    cin >> size;
+
+    char* str = new char[size + 1];
+
+    cout << "Enter the string: ";
+    cin >> str;
+
+    cout << "Reversed string: ";
+
+    for (int i = strlen(str) - 1; i >= 0; i--) {
+        cout << str[i];
+    }
+
+    cout << endl;
+
+    delete[] str;
+
+    return 0;
+}
