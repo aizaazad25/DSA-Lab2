4a1433272cdfcf2087693500a5b98f60a7194bf9
Aiza Azad <aizaazad25@gmail.com>
2026-09-18 10:04:00 +0500

    Add initial tests for isSorted function

---
+++ isSorted.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cassert>
+using namespace std;
+
+// Temporary incorrect implementation
+bool isSorted(const int* arr, const int size) {
+    return false;
+}
+
+// Test 1: Sorted ascending
+void testSortedArray() {
+    int arr[] = {1, 2, 3, 4, 5};
+
+    bool result = isSorted(arr, 5);
+
+    assert(result == true);
+}
+
+// Test 2: Unsorted array
+void testUnsortedArray() {
+    int arr[] = {1, 4, 3, 5, 6};
+
+    bool result = isSorted(arr, 5);
+
+    assert(result == false);
+}
+
+int main() {
+
+    testSortedArray();
+    testUnsortedArray();
+
+    cout << "All tests passed!" << endl;
+
+    return 0;
+}
